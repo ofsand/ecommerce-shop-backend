fa1f62944cdb496242d72391bc47915fc18b5031
ofsand <zesoufian@gmail.com>
2022-08-14 05:16:48 +0100

    Modify user's model
    add  attributes and enable virtuals to convert _id to id

--- models/user.js
+++ models/user.js
@@ -1,10 +1,47 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    username: String,
-    email: String
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    adress: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: Number,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true
+    }
 })
+
+//changing _id to id (removing the underscore) Using Virtuals
+productSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+productSchema.set('toJSON', {
+    virtuals: true
+});
 
 exports.User = mongoose.model("User", userSchema);
