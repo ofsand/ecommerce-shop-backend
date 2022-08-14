5b2f6f1506a86c2da16c5cdb01cbef28c9a48dbd
ofsand <zesoufian@gmail.com>
2022-08-14 05:32:36 +0100

    Remove phone ducpliaction

--- models/user.js
+++ models/user.js
@@ -25,10 +25,6 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
     },
-    phone: {
-        type: Number,
-        required: true,
-    },
     isAdmin: {
         type: Boolean,
         required: true
