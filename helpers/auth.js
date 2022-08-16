cc9393ec572f520a5c0d56c4db5d70068a771a32
ofsand <zesoufian@gmail.com>
2022-08-16 20:02:48 +0100

    Add JWT Validation

--- helpers/auth.js
+++ helpers/auth.js
@@ -2,15 +2,28 @@ const jwt = require("jsonwebtoken");
 
 module.exports = () => {
     return (req, res, next) => {
-        try {
-            const token = req.headers("Authorization");
-            if (!token) return res.status(403).send("Access denied.");
+        //try {
+            //Find JWT in Headers
+            const token = req.headers["authorization"];
+            if (!token) {
+                return res.status(401).send("Access denied.");
+            }else {
+                //Validate JWT
+                const tokenBody = token.slice(7);
+                jwt.verify(tokenBody, process.env.secret, (err, decoded) => {
+                    if(err) {
+                        console.log(`JWT Error: ${err}`)
+                        return res.status(401).send("Error: Access Denied !");
+                    }                    
+                });
+                // No Error, JWT is Good
 
-            const decoded = jwt.verify(token, process.env.secret);
-            req.user = decoded;
-            next();
+                next();
+            }
+/*
         } catch (error) {
             res.status(400).send("Invalid token");
         }
+        */
     };
 };
