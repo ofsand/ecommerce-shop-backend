220dd22df5a72f2f628156f74ca517fb585dc159
ofsand <zesoufian@gmail.com>
2022-08-25 22:58:33 +0100

    convert status type

--- models/order.js
+++ models/order.js
@@ -25,7 +25,7 @@ const orderSchema = mongoose.Schema({
     status: {
         type: String,
         required: true,
-        default: 'Pending',
+        default: 0,
     },
     totalPrice: {
         type: Number,
