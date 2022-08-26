91d23a6ebd4f03e1ea177b7413758552b5a79a20
ofsand <zesoufian@gmail.com>
2022-08-26 02:50:00 +0100

    set default value of status as 0/ Pending

--- models/order.js
+++ models/order.js
@@ -25,7 +25,7 @@ const orderSchema = mongoose.Schema({
     status: {
         type: String,
         required: true,
-        default: 0,
+        default: '0',
     },
     totalPrice: {
         type: Number,
